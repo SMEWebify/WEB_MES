170f9c04ebf180f97aa609dd929a90cb88f9bd57
billyboy35 <kevin.niglaut@wanadoo.fr>
2021-03-15 18:43:25 +0100

    Start task work

---
+++ app/Planning/task.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Planning;
+use \App\SQL;
+
+class Task Extends SQL  {
+
+
+    Public $id;
+    Public $ARTICLE_ID;
+    Public $ORDRE;
+    Public $PRESTA_ID;
+    Public $LABEL;
+    Public $TPS_PREP;
+    Public $TPS_PRO;
+    Public $COUT;
+    Public $PRIX;
+    Public $ETAT;
+    Public $START_TIMESTAMPS;
+    Public $END_START_TIMESTAMPS;
+    
+
+}
